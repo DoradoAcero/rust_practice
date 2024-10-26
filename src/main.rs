fd024015cabcb7e6c54f6d864ee840bc82c52b4c
DoradoAcero <lachlan.alsop@gmail.com>
2024-10-26 20:49:36 +1300

    some basic parts working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::fs;
+use std::{collections::{btree_map::Range, HashMap}, fs};
 
+mod wordle_utils;
+
+use wordle_utils::{initalize_game_state, wordle_compare, GameState, LetterStatus};
 
 fn main() {
     let file_path = "words.txt";
@@ -11,5 +14,30 @@ fn main() {
 
     let target_word = words[rand::random::<usize>() % words.len()];
 
+    let mut game_state = GameState {
+        target_word,
+        letter_matches: HashMap::new(),
+    };
+    // TODO I hate this, but we are going to figure it out later
+    let mut game_state = initalize_game_state(&mut game_state);
+
+    println!("target: {}", target_word);
+    for i in 0..5 {
+        let guess = words[rand::random::<usize>() % words.len()];
+        println!("guess: {}", guess);
+
+        let mut game_state = wordle_compare(&mut game_state, guess);
+
+        for (chr, letter) in game_state.letter_matches.iter() {
+            match letter.status {
+                LetterStatus::Unknown => {
+                    continue;
+                }
+                _ => {
+                    println!("{}: {}", chr, letter.status)
+                }
+            }
+        }
+    }
     
 }
--- src/wordle_utils.rs
+++ src/wordle_utils.rs
@@ -1,8 +1,83 @@
+use core::fmt;
+use std::{collections::HashMap, fmt::Display, iter::Enumerate};
 
 
-fn wordle_compare(target: &str, guess: &str) -> (u32, u32) {
-    let mut target_chars: Vec<char> = target.chars().collect();
-    let mut guess_chars: Vec<char> = guess.chars().collect();
-    let mut correct = 0;
-    let mut misplaced = 0;
+pub enum LetterStatus {
+    LetterMatch,
+    WordMatch,
+    NoMatch,
+    Unknown,
+}
+
+impl Display for LetterStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            LetterStatus::LetterMatch => write!(f, "LetterMatch"),
+            LetterStatus::WordMatch => write!(f, "WordMatch"),
+            LetterStatus::NoMatch => write!(f, "NoMatch"),
+            LetterStatus::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+pub struct Letter {
+    pub(crate) status: LetterStatus,
+    pub(crate) known_position: Option<u32>, // I don't want to do any crazy enum typing quite yet, ill get it running first, then I can do types properly
+    // i8, as I hope the compiler will figure out that we can stack the entire array nicely into a single mem block
+    // TODO: chad shit only, look into if this is actually how it does everything once compiled(i can't read assembly though, so that is a far down the road todo)
+    pub(crate) false_positions: Option<[i8; 4]>, // there are a max of 4 wrong positions for a known letter
+}
+
+pub struct GameState<'a> {
+    pub(crate) target_word: &'a str,
+    pub(crate) letter_matches: HashMap<char, Letter>
+}
+
+pub fn initalize_game_state<'a>(game_state: &'a mut GameState<'a>) -> &mut GameState<'a> {
+    for i in 97u8..=122 {
+        game_state.letter_matches.insert(i as char, Letter{
+            status: LetterStatus::Unknown,
+            known_position: None,
+            false_positions: None,
+        });
+    };
+    
+    game_state
+}
+
+pub fn wordle_compare<'a>(game_state: &'a mut GameState<'a>, guess: &'a str) -> &'a mut GameState<'a> {
+    let target_chars: Vec<char> = game_state.target_word.chars().collect();
+    let guess_chars: Vec<char> = guess.chars().collect();
+    for (i, letter) in guess_chars.iter().enumerate(){
+        if letter == &target_chars[i] {
+            *game_state.letter_matches.get_mut(letter).unwrap() = Letter{
+                status: LetterStatus::LetterMatch,
+                known_position: Some(i as u32),
+                false_positions: None,
+            };
+        } else if target_chars.contains(letter) {
+            let mut existing_false_positions = game_state.letter_matches.get(letter).unwrap().false_positions.unwrap_or([0,0,0,0]);
+            for existing_false_position in existing_false_positions.iter_mut() {
+                if *existing_false_position == i as i8 { // if the location is already known, skip
+                    break
+                }
+                if *existing_false_position == 0 { // if the next location is unknown, set a known incorrect position
+                    *existing_false_position = i as i8;
+                }
+            }
+            *game_state.letter_matches.get_mut(letter).unwrap() = Letter{
+                status: LetterStatus::WordMatch,
+                known_position: None,
+                false_positions: Some(existing_false_positions),
+            };
+        } else {
+            *game_state.letter_matches.get_mut(letter).unwrap() = Letter{
+                status: LetterStatus::NoMatch,
+                known_position: None,
+                false_positions: None,
+            }
+        };
+    };
+
+    game_state
 }
